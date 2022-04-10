2e88b0f3259bbfeb267daa4de109f02931c65a6d
cloverj20 <youly209@gmail.com>
2022-04-10 18:12:34 +0900

    dfaf

---
+++ hello-react/src/component/IterationSample.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClick = () => {
+    const nextName = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextName);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  return (
+    <div>
+      <input value={inputText} onChange={onChange}></input>
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </div>
+  );
+};
+
+export default IterationSample;
