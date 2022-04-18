42a20009c376a35d28b74dea586494ffa60b04cc
cloverj20 <youly209@gmail.com>
2022-04-18 23:31:40 +0900

    ch18 ongoing

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SampleContainer from "./container/SampleContainer";
+
+const App = () => {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+};
+
+export default App;
